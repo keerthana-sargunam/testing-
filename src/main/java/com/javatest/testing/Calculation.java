27cc3f4e1fbb9803f6ecf30fd170fec6436b1f63
sanjay186 <sanjay.infortech@gmail.com>
2019-05-13 13:03:29 +0800

    code changes

--- src/main/java/com/javatest/testing/Calculation.java
+++ src/main/java/com/javatest/testing/Calculation.java
@@ -3,7 +3,7 @@ package com.javatest.testing;
 public class Calculation {
 
 	public static int findMax(int arr[]){  
-        int max=0;  
+        int max=arr[0];  
         for(int i=1;i<arr.length;i++){  
             if(max<arr[i])  
                 max=arr[i];  
